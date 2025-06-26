c1b85e96d5d5e7659c06693c1820c71854d104bb
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2025-06-25 23:04:39 -0400

    Créer la table Utilisateur

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'telephone' => fake()->phoneNumber(),
+            'numero_employe' =>fake()->unique()->randomNumber(5),
+            'travail' => fake()->jobTitle(),
+            'departement' => fake()->jobTitle(),
+            'departement_surveillance' => fake()->jobTitle(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        User::factory(2)->create();
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'telephone' => '1234567890',
+            'numero_employe' => 1,
+            'travail' => 'Job',
+            'departement' => 'Département',
+            'departement_surveillance' => 'Departement de surveillance',
         ]);
     }
 }
