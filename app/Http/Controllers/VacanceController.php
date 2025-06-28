8c7051bed5734084eee9b1e4c84526584c943e82
stanislas musoz <2150901@cegepoutaouais.qc.ca>
2025-06-27 22:31:29 -0400

    méthode CRUD sont terminées pour Vacance

--- app/Http/Controllers/VacanceController.php
+++ app/Http/Controllers/VacanceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Vacance;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreVacanceRequest;
+use App\Http\Requests\VacanceRequest;
 use App\Http\Requests\UpdateVacanceRequest;
 
 class VacanceController extends Controller
@@ -28,9 +28,14 @@ class VacanceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreVacanceRequest $request)
+    public function store(VacanceRequest $request)
     {
-        //
+        Vacance::create([
+            "debut_vacance" => $request->input('debut_vacance'),
+            "fin_vacance" => $request->input('fin_vacance'),
+            "id_employe" => $request->input('id_employe'),
+        ]);
+        return redirect()->route("vacances.index");
     }
 
     /**
@@ -52,9 +57,15 @@ class VacanceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateVacanceRequest $request, Vacance $vacance)
+    public function update(VacanceRequest $request, Vacance $vacance)
     {
-        //
+        $vacance->update([
+            "debut_vacance"=>$request->get('debut_vacance'),
+            "fin_vacance"=>$request->get('fin_vacance'),
+            "id_employe"=>$request->get('id_employe'),
+        ]);
+        $vacance->save();
+        return redirect()->route('vacances.index');
     }
 
     /**
@@ -62,6 +73,7 @@ class VacanceController extends Controller
      */
     public function destroy(Vacance $vacance)
     {
-        //
+        $vacance->delete();
+        return redirect()->route('vacances.index');
     }
 }
